7e0b540554936765686a0d7c8052c4e090368d57
Nikhil Choudhary <nikhil@parthean.com>
2021-05-01 20:36:28 -0400

    build with the basics working

--- public/caption/custom.js
+++ public/caption/custom.js
@@ -1,55 +1,66 @@
 /*global chrome*/
 
-const replacementAndEducationalModal = `
-<span class="calledout">
-    <p class="replacement">Fam</p>
-    <div class="education-modal">
-        <p class="replaced-word-header">Replaced <strong class="actual-word-used">China Virus</strong> with COVID-19</p>
-        <i class="one-line-explainer">Calling COVID-19 the “Wuhan Virus” or “China Virus” is inaccurate and xenophobic</i>
+expandEducationalModal = () => {
+    document.getElementById('education-modal').style.width = '350px'
+    const addContent = () => {
+        document.getElementById('more-educational-material').innerHTML = `
+        <p class="one-line-explainer">Calling COVID-19 the “Wuhan Virus” or “China Virus” is inaccurate and xenophobic</p>
         <p class="deeper-explainer"><strong>DO:</strong> Talk about the new coronavirus disease (COVID-19)</p>
         <p class="deeper-explainer"><strong>DO NOT:</strong> Attach locations or ethnicity to the disease; this is not a “Wuhan Virus,” “Chinese Virus,” or “Asian Virus.” The official name for the disease was deliberately chosen to avoid stigmatization.</p>
         <p class="learn-more">Learn more <a class="learn-more-link" target="_blank" href="https://medicine.yale.edu/news-article/calling-covid-19-the-wuhan-virus-or-china-virus-is-inaccurate-and-xenophobic/">here</a></p>
+        `
+    }
+    setTimeout(addContent, 100)
+}
+
+const replacementAndEducationalModal = `
+<span class="calledout">
+    <p class="replacement">COVID-19</p>
+    <div class="education-modal" id="education-modal">
+        <div class="education-modal-starter">
+            <p>Replaced:</p>
+            <strong class="actual-word-used">China Virus</strong>
+            <div class="learn-more-prompt">
+                <img src="https://i.imgur.com/XITiHDy.jpg" width="30px" />
+                <button id="down-arrow-show-more">Learn Why &#10549;</button>
+            </div>
+        </div>
+        <div class="learn-more-clicked" id="more-educational-material">            
+        </div>
     </div>
 </span>`
 
-//Replacing the word "flu" separately by the tags h3,h1, and p the common text tags on Reddit.
-//Could probably write a more efficient function to combine these three...
+let runBefore = false
+
 function replace() {
+    if (runBefore) {
+        return
+    }
+    runBefore = true
     console.time('replace')
-    var allHeaderThreeTags = document.getElementsByTagName('h3')
-    var innerHeaderThree
-    for (var i = 0; i < allHeaderThreeTags.length; i++) {
-        innerHeaderThree = allHeaderThreeTags[i].innerHTML
-        innerHeaderThree = innerHeaderThree.replace(
-            /Flu/gi,
+    const allTags = [
+        ...document.getElementsByTagName('h3'),
+        ...document.getElementsByTagName('h1'),
+        ...document.getElementsByTagName('p'),
+    ]
+    var content
+    for (var i = 0; i < allTags.length; i++) {
+        content = allTags[i].innerHTML
+        content = content.replace(
+            /China\sVirus/gi,
             replacementAndEducationalModal
         )
-        allHeaderThreeTags[i].innerHTML = innerHeaderThree
-    }
 
-    var allHeaderOneTags = document.getElementsByTagName('h1')
-    var innerHeaderOne
-    for (var i = 0; i < allHeaderOneTags.length; i++) {
-        innerHeaderOne = allHeaderOneTags[i].innerHTML
-        innerHeaderOne = innerHeaderOne.replace(
-            /Flu/gi,
-            replacementAndEducationalModal
-        )
-        allHeaderOneTags[i].innerHTML = innerHeaderOne
+        allTags[i].innerHTML = content
     }
-
-    var allParagraphTags = document.getElementsByTagName('p')
-    var innerParagraph
-    for (var i = 0; i < allParagraphTags.length; i++) {
-        innerParagraph = allParagraphTags[i].innerHTML
-        innerParagraph = innerParagraph.replace(
-            /Flu/gi,
-            replacementAndEducationalModal
-        )
-        allParagraphTags[i].innerHTML = innerParagraph
-    }
+    document
+        .getElementById('down-arrow-show-more')
+        .addEventListener('click', expandEducationalModal)
     console.timeEnd('replace')
 }
 
 //Wait for the page to load before running the function below
 window.addEventListener('load', replace)
+
+// ...or just get it to run after a short amount of time
+setTimeout(replace, 1000)
